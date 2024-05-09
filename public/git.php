eef6986fe45ba5717bfa34a99125a937470286c1
Aleksandr Martianov <a.p.martianov@gmail.com>
2024-05-09 14:36:55 +0300

    добавил страницу git

---
+++ public/git.php
@@ -0,0 +1,17 @@
+<?php
+echo "<h3>Инициализация</h3>\n";
+echo "git init <br>\n";
+echo "git status \n";
+echo "<h3>Запушить</h3>\n";
+echo "git add . <br>\n";
+echo "git commit -m \"Describe the changes you made\" <br>\n";
+echo "git push origin main\n";
+echo "<h3>Забрать</h3>\n";
+echo "git pull <br>\n";
+echo "git pull origin main \n";
+echo "<h3>Клонировать</h3>\n";
+echo "git clone <a href=\"https://github.com/martia-by/auth-php\" target=\"_blank\">https://github.com/martia-by/auth-php</a> <br>\n";
+echo "<h3>Настроить</h3>\n";
+echo "git config --list <br>\n";
+echo "git config --global user.name \"My Name\" <br>\n";
+echo "git config --global user.email my@email <br>\n";
